dc610206ef184428a00162e6465ce1e09c62ff48
CiaranMcAndrew <ciaranmc@gmail.com>
2024-04-09 17:20:28 +0100

    scp files

--- controller/scripts/setup-controller.sh
+++ controller/scripts/setup-controller.sh
@@ -11,6 +11,21 @@ cd
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 ssh-keygen -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
+
+# Copy ssh key to nodes
 sshpass -p "$NODEPASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub pi@node01.local
 sshpass -p "$NODEPASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub pi@node02.local
+
+# Copy files to nodes
+scp -r ../../node/* pi@node01.local:/
+scp -r ../../node/* pi@node02.local:/
+
+# Ping nodes using ansible
+ansible cube -m ping
+
+# Setup iptables
+echo "$NODEPASSWORD" | ansible cube -m apt -a "name=iptables state=present" --become -K
+
+# Reboot nodes
+ansible workers -b -m shell -a "reboot"
 .
