b3fcec2a9a3f132c527d324d23ecfe440852ca54
CiaranMcAndrew <ciaranmc@gmail.com>
2024-04-11 23:43:33 +0100

    Add option to change number of nodes as input

--- controller/scripts/setup-controller.sh
+++ controller/scripts/setup-controller.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Define defautl options
+NODES=2
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -n|--nodes)
+      NODES="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
 sudo apt-get install -y pipx
 sudo apt-get install -y sshpass
 pipx ensurepath
@@ -14,17 +31,15 @@ chmod 700 ~/.ssh
 ssh-keygen -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
 
 # Copy ssh key to nodes
-for i in $(seq 1 10); 
+for i in $(seq 1 $NODES); 
     do {
         node=$(printf 'node%02d\n' "$i")
-        echo "Copying ssh key to node: $node"
-        sshpass -p "$NODEPASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub "pi@$node.local"
+        echo "Copying ssh key to node: $NAME"
+        sshpass -p "$NODEPASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub "pi@$NAME.local"
      } || {
-        echo "Node: $node not available"
+        echo "Node: $NAME not available"
     }
 done
-#sshpass -p "$NODEPASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub pi@node01.local
-#sshpass -p "$NODEPASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub pi@node02.local
 
 # Copy files to nodes
 #scp -r ../../node/ pi@node01.local:/
